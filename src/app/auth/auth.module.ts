77e3296c4fffb783f547dbc482b0b12e56356603
Andranik Antonyan <andantonyan@gmail.com>
2018-05-31 02:10:44 +0400

    ng generate feature auth/Auth -m auth --group

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromAuth from './reducers/auth.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './effects/auth.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    StoreModule.forFeature('auth', fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects])
   ],
   declarations: []
 })
