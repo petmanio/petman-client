779396fd422101034c521d388ef221f00ad33cbd
Andranik Antonyan <andantonyan@gmail.com>
2018-07-08 01:37:36 +0400

    chore(card): ellipsis

--- src/app/modules/shared/pipes/ellipsis/ellipsis.pipe.ts
+++ src/app/modules/shared/pipes/ellipsis/ellipsis.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'appEllipsis' })
 export class EllipsisPipe implements PipeTransform {
-  transform(str: string, limit: number = 50) {
+  transform(str: string, limit: number = 100) {
     str = str || '';
     const withoutHtml = str.replace(/(<([^>]+)>)/ig, '');
 
