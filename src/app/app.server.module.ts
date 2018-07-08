19d04cfcda605390339302bd9f546fd13707ad39
Andranik Antonyan <andantonyan@gmail.com>
2018-07-08 23:33:27 +0400

    chore(angular): build configs

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { RouterModule, Routes } from '@angular/router';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
 import { AppShellComponent } from './app-shell/app-shell.component';
 
 const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent }];
 
 @NgModule({
   imports: [
-    AppModule,
     ServerModule,
+    ServerTransferStateModule,
+    ModuleMapLoaderModule,
     RouterModule.forRoot(routes),
+
+    AppModule
   ],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  api: '',
+  gaId: '',
+  fb: {
+    appId: '782744788540027',
+    scope: 'public_profile,email'
+  },
+  mapApiKey: 'AIzaSyCjMDwYXmx_jeTJ2Ha2rM7_V2YBxcA7Gv4',
 };
