725e526dbfbcffb689879b4cc62b6b535da96642
Andranik Antonyan <andantonyan@gmail.com>
2018-07-14 20:26:32 +0400

    chore(environment): fb app id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   api: 'http://localhost:3000',
   gaId: '',
   fb: {
-    appId: '782744788540027',
+    appId: '614253672289323',
     scope: 'public_profile,email'
   },
   mapApiKey: 'AIzaSyCjMDwYXmx_jeTJ2Ha2rM7_V2YBxcA7Gv4',
