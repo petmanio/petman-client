bce06dac8c9d13a667ffb1d1b4cb47e893456413
Andranik Antonyan <andantonyan@gmail.com>
2018-07-23 01:15:46 +0400

    chore(icons): optimize

--- src/icons.ts
+++ src/icons.ts
@@ -1,34 +1,30 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-import {
-  faFacebookF,
-  faFacebookMessenger,
-  faGooglePlusG,
-  faLinkedinIn,
-  faPinterestP,
-  faRedditAlien,
-  faStumbleupon,
-  faTelegramPlane,
-  faTumblr,
-  faTwitter,
-  faVk,
-  faWhatsapp,
-  faXing
-} from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons/faFacebookMessenger';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons/faGooglePlusG';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faPinterestP } from '@fortawesome/free-brands-svg-icons/faPinterestP';
+import { faRedditAlien } from '@fortawesome/free-brands-svg-icons/faRedditAlien';
+import { faStumbleupon } from '@fortawesome/free-brands-svg-icons/faStumbleupon';
+import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons/faTelegramPlane';
+import { faTumblr } from '@fortawesome/free-brands-svg-icons/faTumblr';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faVk } from '@fortawesome/free-brands-svg-icons/faVk';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+import { faXing } from '@fortawesome/free-brands-svg-icons/faXing';
 
-import {
-  faCheck,
-  faCommentAlt,
-  faEllipsisH,
-  faEnvelope,
-  faExclamation,
-  faLink,
-  faMinus,
-  faMoneyBillAlt,
-  faPrint,
-  faTimesCircle,
-  faPhone
-} from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons/faCommentAlt';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faExclamation } from '@fortawesome/free-solid-svg-icons/faExclamation';
+import { faLink } from '@fortawesome/free-solid-svg-icons/faLink';
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus';
+import { faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons/faMoneyBillAlt';
+import { faPrint } from '@fortawesome/free-solid-svg-icons/faPrint';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 
 const icons = [
   faFacebookF,
@@ -54,7 +50,7 @@ const icons = [
   faMinus,
   faMoneyBillAlt,
   faTimesCircle,
-  faPhone,
+  faPhone
 ];
 
 library.add(...icons);
